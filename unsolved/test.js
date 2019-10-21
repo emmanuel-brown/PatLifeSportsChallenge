371aa102817273ac549b9d5fffc8dd38c4377689
Emmanuel Oklo Brown <emmanuelbrown1310@gmail.com>
2019-10-21 11:03:02 -0400

    starter pack

--- unsolved/test.js
+++
@@ -1,5 +0,0 @@
-const names = ["emma", "Emmanuel", "jackson", "Nathan", "Dora", "Jasmin"]
-const exercises = ["lifting", "running", "swimming", "bascketball"]
-const stuff = []
-names.map(name =>  stuff.push({username: name}))
-console.log(stuff);
