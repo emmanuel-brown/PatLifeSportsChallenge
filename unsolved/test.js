d0fc581633c2f2f0790525aa2ecc302eb11aa3d8
Emmanuel Oklo Brown <emmanuelbrown1310@gmail.com>
2019-10-21 10:33:55 -0400

    manny

---
+++ unsolved/test.js
@@ -0,0 +1,5 @@
+const names = ["emma", "Emmanuel", "jackson", "Nathan", "Dora", "Jasmin"]
+const exercises = ["lifting", "running", "swimming", "bascketball"]
+const stuff = []
+names.map(name =>  stuff.push({username: name}))
+console.log(stuff);
